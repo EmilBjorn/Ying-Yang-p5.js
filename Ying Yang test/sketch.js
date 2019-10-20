cf700e2f3126ccf6fce5ea540c8bf4acf1ee04a3
EmilBjorn <bjoern.emil@gmail.com>
2019-10-20 22:08:30 +0200

    Add files via upload

--- Ying Yang test/sketch.js
+++ Ying Yang test/sketch.js
@@ -1,18 +1,33 @@
-let x = 1;
-let vx = 10;
+let ang = 0;
+let r = 50;
 
 function setup() {
-	createCanvas(600, 600);
+  createCanvas(600, 600);
 }
 
 function draw() {
-	background(100, 140, 200);
-	translate(canvas.width / 2, canvas.height / 2)
-	fill(0, 0, 100);
-	noStroke();
-	ellipse(0, 0, 40);
-	/* x += vx
-	if (x > canvas.width || x < 0) {
-		vx *= -1
-	} */
+  background(100, 140, 200);
+  translate(canvas.width / 2, canvas.height / 2);
+  noStroke();
+
+  let x = cos(ang) * r;
+  let y = sin(ang) * r;
+  let ang2 = ang + PI;
+  let x2 = cos(ang2) * r;
+  let y2 = sin(ang2) * r;
+
+  fill(255, 255, 255);
+
+  arc(0, 0, r * 4, r * 4, ang + PI, ang);
+
+  fill(0, 0, 0);
+  arc(0, 0, r * 4, r * 4, ang2 + PI, ang2);
+  fill(255, 255, 255);
+  ellipse(x, y, 2 * r, 2 * r);
+
+  fill(0, 0, 0);
+
+  ellipse(x2, y2, 2 * r, 2 * r);
+
+  ang += 0.025;
 }

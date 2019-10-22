0753c1ab8cf98b5e83b7b8529bb4b8017d1a97b1
Emil Bjørn <bjoern.emil@gmail.com>
2019-10-23 00:29:56 +0200

    finished animation

--- Ying Yang test/sketch.js
+++ Ying Yang test/sketch.js
@@ -5,9 +5,10 @@ let Y1;
 
 function setup() {
   createCanvas(600, 600);
-  Y1 = new Yinyang(-100, 0, 50);
-  Y2 = new Yinyang(100, 0, 50);
   ang0 = createVector(1, 0);
+  Y1 = new Yinyang(-100, 0, r, 2 * r, -1);
+  Y2 = new Yinyang(100, 0, r, 2 * r, -1);
+  Y3 = new Yinyang(0, 0, 2 * r, 0, 1);
 }
 
 function draw() {
@@ -15,42 +16,83 @@ function draw() {
   translate(canvas.width / 2, canvas.height / 2);
   noStroke();
 
-  // Y1.move();
-  // Y2.move();
+  Y1.move();
+  Y2.move();
+  Y3.move();
+  Y3.show();
   Y1.show();
   Y2.show();
-
-
-
 }
 
 class Yinyang {
-  constructor(x, y, r) {
-    this.vector = createVector(x, y)
-    // this.ang = this.vector.angleBetween(ang0)
-    this.ang = 0:
+  constructor(x, y, size, angRadius = 2 * r, clockwise) {
+    this.vector = createVector(x, y);
+    this.ang = this.vector.angleBetween(ang0);
+    if (isNaN(this.ang)) {
+      this.ang = 0;
+    }
     this.rot = 0;
-    // this.x = x;
-    // this.y = y;
-    this.r = r;
-    console.log(this.x, this.ang)
+    this.r = size;
+    this.dist = angRadius;
+    this.dir = clockwise;
   }
 
   move() {
-    this.rot += 0.03;
-    this.ang += 0.015;
-    this.vector.x = this.r * sin(this.ang);
-    this.vector.y = this.r * cos(this.ang);
+    this.rot += 0.01 * this.dir;
+    this.ang -= 0.01 * this.dir;
+    this.vector.x = this.dist * cos(this.ang);
+    this.vector.y = this.dist * sin(this.ang);
   }
 
   show() {
-
+    // Draws the shapes of the yin yang
     fill(255);
-    arc(this.vector.x, this.vector.y, r * 4, r * 4, this.rot, this.rot + PI);
+    arc(
+      // White half-circle
+      this.vector.x,
+      this.vector.y,
+      this.r * 4,
+      this.r * 4,
+      this.rot,
+      this.rot + PI
+    );
     fill(0);
-    arc(this.vector.x, this.vector.y, r * 4, r * 4, this.rot + PI, this.rot + PI + PI);
-    ellipse(this.vector.x + r * cos(this.rot), this.vector.y + r * sin(this.rot), r * 2)
-    fill(255)
-    ellipse(this.vector.x - r * cos(this.rot), this.vector.y - r * sin(this.rot), r * 2)
+    arc(
+      // Black half-circle
+      this.vector.x,
+      this.vector.y,
+      this.r * 4,
+      this.r * 4,
+      this.rot + PI,
+      this.rot + PI + PI
+    );
+    if (this.dir == -1) {
+      ellipse(
+        // Black yang
+        this.vector.x - this.r * cos(this.rot),
+        this.vector.y - this.r * sin(this.rot),
+        this.r * 2
+      );
+      fill(255);
+      ellipse(
+        // White yin
+        this.vector.x + this.r * cos(this.rot),
+        this.vector.y + this.r * sin(this.rot),
+        this.r * 2
+      );
+    }
+    ellipse(
+      // White inner circle
+      this.vector.x - this.r * cos(this.rot),
+      this.vector.y - this.r * sin(this.rot),
+      this.r
+    );
+    fill(0);
+    ellipse(
+      // Black inner circle
+      this.vector.x + this.r * cos(this.rot),
+      this.vector.y + this.r * sin(this.rot),
+      this.r
+    );
   }
 }

c3300d9b3fe2872caf6c096f9689d644c6357d5d
Emil Bjørn <bjoern.emil@gmail.com>
2019-10-22 18:44:19 +0200

    Yinyang Class extended. need to make them move, not just rotate)

--- Ying Yang test/sketch.js
+++ Ying Yang test/sketch.js
@@ -1,8 +1,13 @@
 let ang = 0;
 let r = 50;
+let ang0;
+let Y1;
 
 function setup() {
   createCanvas(600, 600);
+  Y1 = new Yinyang(-100, 0, 50);
+  Y2 = new Yinyang(100, 0, 50);
+  ang0 = createVector(1, 0);
 }
 
 function draw() {
@@ -10,32 +15,42 @@ function draw() {
   translate(canvas.width / 2, canvas.height / 2);
   noStroke();
 
-  let x = cos(ang) * r;
-  let y = sin(ang) * r;
-  let ang2 = ang + PI;
-  let x2 = cos(ang2) * r;
-  let y2 = sin(ang2) * r;
+  // Y1.move();
+  // Y2.move();
+  Y1.show();
+  Y2.show();
 
-  fill(255, 255, 255);
 
-  arc(0, 0, r * 4, r * 4, ang + PI, ang);
 
-  fill(0, 0, 0);
-  arc(0, 0, r * 4, r * 4, ang2 + PI, ang2);
-  fill(255, 255, 255);
-  ellipse(x, y, 2 * r, 2 * r);
-
-  fill(0, 0, 0);
-
-  ellipse(x2, y2, 2 * r, 2 * r);
-
-  ang += 0.035;
 }
 
 class Yinyang {
-  constructor(x, y, r, ang) {
-    this.x = x;
-    this.y = y;
-    this.ang = ang;
+  constructor(x, y, r) {
+    this.vector = createVector(x, y)
+    // this.ang = this.vector.angleBetween(ang0)
+    this.ang = 0:
+    this.rot = 0;
+    // this.x = x;
+    // this.y = y;
+    this.r = r;
+    console.log(this.x, this.ang)
+  }
+
+  move() {
+    this.rot += 0.03;
+    this.ang += 0.015;
+    this.vector.x = this.r * sin(this.ang);
+    this.vector.y = this.r * cos(this.ang);
+  }
+
+  show() {
+
+    fill(255);
+    arc(this.vector.x, this.vector.y, r * 4, r * 4, this.rot, this.rot + PI);
+    fill(0);
+    arc(this.vector.x, this.vector.y, r * 4, r * 4, this.rot + PI, this.rot + PI + PI);
+    ellipse(this.vector.x + r * cos(this.rot), this.vector.y + r * sin(this.rot), r * 2)
+    fill(255)
+    ellipse(this.vector.x - r * cos(this.rot), this.vector.y - r * sin(this.rot), r * 2)
   }
 }
